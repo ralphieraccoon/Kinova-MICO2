776f30ed27421b02131918633874aad6dbdd3401
ralphieraccoon <damian.crosby@manchester.ac.uk>
2021-01-15 18:23:51 +0000

    Major reorganisation + added and .so output for myRIO.

---
+++ lib/shared/klinterface.cpp
@@ -0,0 +1,265 @@
+#ifdef _DLL
+
+#include <Windows.h>
+#include <conio.h>
+#include "../KLInterface_dll/pch.h"
+
+#else
+
+#include <dlfcn.h>
+
+#endif
+
+#include "klinterface.h"
+#include <iostream>
+#include <stdlib.h>
+
+#ifdef _DLL
+
+HINSTANCE commandLayer_handle;
+
+#else
+
+void* commandLayer_handle;
+
+#endif
+
+int robotStatus;
+
+int(*MyInitAPI)();
+int(*MyCloseAPI)();
+int(*MySendBasicTrajectory)(TrajectoryPoint command);
+int(*MyGetDevices)(KinovaDevice devices[MAX_KINOVA_DEVICE], int& result);
+int(*MySetActiveDevice)(KinovaDevice device);
+int(*MyMoveHome)();
+int(*MyInitFingers)();
+int(*MyGetCartesianCommand)(CartesianPosition&);
+int(*MyGetCartesianPosition)(CartesianPosition&);
+int(*MyGetAngularCommand)(AngularPosition&);
+int(*MyGetAngularPosition)(AngularPosition&);
+int(*MyEraseAllTrajectories)();
+
+int open()
+{
+
+#ifdef _DLL
+
+	commandLayer_handle = LoadLibrary(L"CommandLayerWindows.dll");
+
+	//We load the functions from the library (Under Windows, use GetProcAddress)
+	MyInitAPI = (int(*)()) GetProcAddress(commandLayer_handle, "InitAPI");
+	MyCloseAPI = (int(*)()) GetProcAddress(commandLayer_handle, "CloseAPI");
+	MyMoveHome = (int(*)()) GetProcAddress(commandLayer_handle, "MoveHome");
+	MyInitFingers = (int(*)()) GetProcAddress(commandLayer_handle, "InitFingers");
+	MyGetDevices = (int(*)(KinovaDevice devices[MAX_KINOVA_DEVICE], int& result)) GetProcAddress(commandLayer_handle, "GetDevices");
+	MySetActiveDevice = (int(*)(KinovaDevice devices)) GetProcAddress(commandLayer_handle, "SetActiveDevice");
+	MySendBasicTrajectory = (int(*)(TrajectoryPoint)) GetProcAddress(commandLayer_handle, "SendBasicTrajectory");
+	MyGetCartesianCommand = (int(*)(CartesianPosition&)) GetProcAddress(commandLayer_handle, "GetCartesianCommand");
+	MyGetCartesianPosition = (int(*)(CartesianPosition&)) GetProcAddress(commandLayer_handle, "GetCartesianPosition");
+	MyGetAngularCommand = (int(*)(AngularPosition&)) GetProcAddress(commandLayer_handle, "GetAngularCommand");
+	MyGetAngularPosition = (int(*)(AngularPosition&)) GetProcAddress(commandLayer_handle, "GetAngularPosition");
+	MyEraseAllTrajectories = (int(*)()) GetProcAddress(commandLayer_handle, "EraseAllTrajectories");
+
+#else
+
+	commandLayer_handle = dlopen("Kinova.API.USBCommandLayerUbuntu.so", RTLD_NOW | RTLD_GLOBAL);
+
+	//We load the functions from the library (Under Windows, use GetProcAddress)
+	MyInitAPI = (int(*)()) dlsym(commandLayer_handle, "InitAPI");
+	MyCloseAPI = (int(*)()) dlsym(commandLayer_handle, "CloseAPI");
+	MyMoveHome = (int(*)()) dlsym(commandLayer_handle, "MoveHome");
+	MyInitFingers = (int(*)()) dlsym(commandLayer_handle, "InitFingers");
+	MyGetDevices = (int(*)(KinovaDevice devices[MAX_KINOVA_DEVICE], int& result)) dlsym(commandLayer_handle, "GetDevices");
+	MySetActiveDevice = (int(*)(KinovaDevice devices)) dlsym(commandLayer_handle, "SetActiveDevice");
+	MySendBasicTrajectory = (int(*)(TrajectoryPoint)) dlsym(commandLayer_handle, "SendBasicTrajectory");
+	MyGetCartesianCommand = (int(*)(CartesianPosition&)) dlsym(commandLayer_handle, "GetCartesianCommand");
+	MyGetCartesianPosition = (int(*)(CartesianPosition&)) dlsym(commandLayer_handle, "GetCartesianPosition");
+	MyGetAngularCommand = (int(*)(AngularPosition&)) dlsym(commandLayer_handle, "GetAngularCommand");
+	MyGetAngularPosition = (int(*)(AngularPosition&)) dlsym(commandLayer_handle, "GetAngularPosition");
+	MyEraseAllTrajectories = (int(*)()) dlsym(commandLayer_handle, "EraseAllTrajectories");
+
+#endif
+
+	//Verify that all functions has been loaded correctly
+	if ((MyInitAPI == NULL) || (MyCloseAPI == NULL) || (MySendBasicTrajectory == NULL) ||
+		(MyGetDevices == NULL) || (MySetActiveDevice == NULL) || (MyGetCartesianCommand == NULL) ||
+		(MyMoveHome == NULL) || (MyInitFingers == NULL) || (MyGetCartesianCommand == NULL) || 
+		(MyGetCartesianPosition == NULL) || (MyGetAngularCommand == NULL) || (MyGetAngularPosition == NULL) ||
+		(MyEraseAllTrajectories == NULL))
+
+	{
+		return -1;
+	}
+	else
+	{
+
+		robotStatus = (*MyInitAPI)();
+		return robotStatus;
+
+	}
+
+}
+
+int close()
+{
+
+	int result;
+
+	result = MyCloseAPI();
+
+#ifdef _DLL
+
+	FreeLibrary(commandLayer_handle);
+
+#else
+
+	dlclose(commandLayer_handle);
+
+#endif
+
+	return result;
+
+}
+
+int getMaxDevices()
+
+{
+
+	return MAX_KINOVA_DEVICE;
+
+}
+
+int getDevices(LVKinovaDevice *devices, int *devicecount)
+{
+
+	KinovaDevice list[MAX_KINOVA_DEVICE];
+
+	*devicecount = MyGetDevices(list, robotStatus);
+
+	LVKinovaDevice result[MAX_KINOVA_DEVICE];
+
+	for (int i = 0; i < MAX_KINOVA_DEVICE; i++)
+	{
+		result[i] = LVKinovaDevice(list[i]);
+	}
+
+	memcpy(devices, result, sizeof(LVKinovaDevice) * MAX_KINOVA_DEVICE);
+
+	return robotStatus;
+
+}
+
+int setActiveDevice(LVKinovaDevice *device)
+{
+
+	KinovaDevice input;
+
+	input = *device;
+
+	return MySetActiveDevice(input);
+
+}
+
+
+int sendBasicTrajectory(LVTrajectoryPoint *point)
+{
+
+	TrajectoryPoint input;
+
+	input = *point;
+
+	return MySendBasicTrajectory(input);
+
+}
+
+int home()
+{
+
+	return MyMoveHome();
+
+}
+
+int initFingers()
+{
+
+	return MyInitFingers();
+
+}
+
+
+int getCartesianCommand(CartesianPosition *position)
+{
+
+	return MyGetCartesianCommand(*position);
+
+}
+
+int getCartesianPosition(CartesianPosition *position)
+{
+
+	return MyGetCartesianPosition(*position);
+
+}
+
+int getAngularCommand(AngularPosition* actuators)
+{
+
+	return MyGetAngularCommand(*actuators);
+
+}
+
+int getAngularPosition(AngularPosition* actuators)
+{
+
+	return MyGetAngularPosition(*actuators);
+
+}
+
+int clearTrajectory()
+{
+
+	return MyEraseAllTrajectories();
+
+}
+
+
+
+char* LStrHandletoCharArray(LStrHandle string) {
+
+	std::string cppString((char*)LStrBuf(*string), 0, LStrLen(*string));
+
+	char result[SERIAL_LENGTH];
+
+#ifdef _DLL
+
+	strcpy_s(result, cppString.c_str());
+
+#else
+
+	strcpy(result, cppString.c_str());
+
+#endif
+
+	return result;
+
+}
+
+LStrHandle CharArraytoLStrHandle(char* arr) {
+
+	std::string cppString(arr);
+
+	LStrHandle result;
+
+#ifdef _DLL
+
+	result = (LStrHandle)DSNewHandle(sizeof(int32) + cppString.size() * sizeof(uChar));
+
+#endif
+
+	cppString.copy((char*)LStrBuf(*result), cppString.size());
+
+	LStrLen(*result) = cppString.size();
+
+	return result;
+
+}
---
+++ lib/shared/klinterface.h
@@ -0,0 +1,190 @@
+#pragma pack(1)
+
+#ifdef _DLL
+
+#include "../KLInterface_dll/CommunicationLayerWindows.h"
+
+#else
+
+#include "../LKInterface_so/Kinova.API.CommLayerUbuntu.h"
+
+#endif
+
+#include "KinovaTypes.h"
+#include "extcode.h"
+#include "fundtypes.h"
+#include "platdefines.h"
+#include "lv_prolog.h"
+#include "lv_epilog.h"
+#include <string.h>
+
+
+#ifdef _DLL
+
+#ifdef KLINTERFACE_EXPORTS
+#define KLINTERFACE_API __declspec(dllexport)
+#else
+#define KLINTERFACE_API __declspec(dllimport)
+#endif
+
+#else
+
+#ifndef KLINTERFACE_API
+#define KLINTERFACE_API __attribute__ ((visibility ("default")))
+#endif
+
+
+#endif
+
+char* LStrHandletoCharArray(LStrHandle string);
+
+LStrHandle CharArraytoLStrHandle(char* arr);
+
+struct LVKinovaDevice {
+
+	LStrHandle SerialNumber;
+
+	LStrHandle Model;
+
+	int32_t VersionMajor;
+
+	int32_t VersionMinor;
+
+	int32_t VersionRelease;
+
+	int32_t DeviceType;
+
+	int32_t DeviceID;
+
+	operator KinovaDevice() const
+
+	{
+
+		KinovaDevice k;
+
+#ifdef _DLL
+
+		strcpy_s(k.SerialNumber, 20, LStrHandletoCharArray(SerialNumber));
+		strcpy_s(k.Model, 20, LStrHandletoCharArray(Model));
+
+#else
+
+		strncpy(k.SerialNumber, LStrHandletoCharArray(SerialNumber), 20);
+		strncpy(k.Model, LStrHandletoCharArray(Model), 20);
+
+#endif 
+
+		k.VersionMajor = VersionMajor;
+		k.VersionMinor = VersionMinor;
+		k.VersionRelease = VersionRelease;
+		k.DeviceType = DeviceType;
+		k.DeviceID = DeviceID;
+
+		return k;
+
+
+	};
+
+	LVKinovaDevice(KinovaDevice kdevice)
+		: SerialNumber(CharArraytoLStrHandle(kdevice.SerialNumber))
+		, Model(CharArraytoLStrHandle(kdevice.Model))
+		, VersionMajor(kdevice.VersionMajor)
+		, VersionMinor(kdevice.VersionMinor)
+		, VersionRelease(kdevice.VersionRelease)
+		, DeviceType(kdevice.DeviceType)
+		, DeviceID(kdevice.DeviceID)
+	{}
+
+	LVKinovaDevice()
+	{}
+
+};
+
+struct LVUserPosition {
+
+	uint16_t Type;
+
+	float Delay;
+
+	CartesianInfo CartesianPosition;
+
+	AngularInfo Actuators;
+
+	uint16_t HandMode;
+
+	FingersPosition Fingers;
+
+	operator UserPosition() const
+
+	{
+
+		UserPosition k;
+
+		k.Type = static_cast<POSITION_TYPE>(Type);
+		k.Delay = Delay;
+		k.CartesianPosition = CartesianPosition;
+		k.Actuators = Actuators;
+		k.HandMode = static_cast<HAND_MODE>(HandMode);
+		k.Fingers = Fingers;
+
+		return k;
+
+	}
+
+};
+
+struct LVTrajectoryPoint {
+
+	LVUserPosition Position;
+
+	LVBoolean LimitationsActive;
+
+	LVBoolean SynchroType;
+
+	Limitation Limitations;
+
+	operator TrajectoryPoint() const
+
+	{
+
+		TrajectoryPoint k;
+
+		k.Position = Position;
+		k.LimitationsActive = LimitationsActive;
+		k.SynchroType = SynchroType;
+		k.Limitations = Limitations;
+
+		return k;
+
+	}
+
+};
+
+extern "C" KLINTERFACE_API int open();
+
+extern "C" KLINTERFACE_API int close();
+
+extern "C" KLINTERFACE_API int getMaxDevices();
+
+extern "C" KLINTERFACE_API int getDevices(LVKinovaDevice *devices, int *devicecount);
+
+extern "C" KLINTERFACE_API int setActiveDevice(LVKinovaDevice *device);
+
+extern "C" KLINTERFACE_API int home();
+
+extern "C" KLINTERFACE_API int sendBasicTrajectory(LVTrajectoryPoint *point);
+
+extern "C" KLINTERFACE_API int initFingers();
+
+extern "C" KLINTERFACE_API int clearTrajectory();
+
+extern "C" KLINTERFACE_API int getCartesianPosition(CartesianPosition *position);
+
+extern "C" KLINTERFACE_API int getAngularPosition(AngularPosition *actuators);
+
+extern "C" KLINTERFACE_API int getCartesianCommand(CartesianPosition *position);
+
+extern "C" KLINTERFACE_API int getAngularCommand(AngularPosition *actuators);
+
+
+
